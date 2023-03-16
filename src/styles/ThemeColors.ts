0264018acc2aef9b13f3e95dba7a911407c4cdbf
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-16 17:51:32 -0400

    feat: add styles of app

---
+++ src/styles/ThemeColors.ts
@@ -0,0 +1,9 @@
+export const ThemeColors = {
+  background: '#FAFAF9',
+  white: '#FFFFFF',
+  inputPlaceholder: '#52525B',
+  inputBackground: '#F4F4F5',
+  inputBorder: '#059669',
+  button: '#16A34A',
+  black: '#000',
+};
---
+++ src/styles/ThemeSize.ts
@@ -0,0 +1,5 @@
+import { PixelRatio } from 'react-native';
+const { getPixelSizeForLayoutSize } = PixelRatio;
+export const ThemeSize = {
+  52: getPixelSizeForLayoutSize(22),
+};
