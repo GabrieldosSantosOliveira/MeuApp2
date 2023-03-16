ef7f74cbe1ec073fbe3685cd3cbe81f2f741d92d
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-16 17:52:47 -0400

    feat: loaded application fonts

--- App.tsx
+++ App.tsx
@@ -1,108 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { useState } from "react";
-type Form = {
-  one: string;
-  dois: string;
-  tres: string;
-  quatro: string;
-  result: string;
-};
-export default function App() {
-  const [input, setInput] = useState<Form>({
-    one: "",
-    dois: "",
-    tres: "",
-    quatro: "X",
-    result: "",
-  });
-  function handleInputChange(e: any) {
-    const { id, value } = e.target;
-    setInput({ ...input, [id]: value });
-  }
-  function handleSubmit(form: any) {
-    form.preventDefault();
-    let valor = parseInt(input.one);
-    let dois = parseInt(input.dois);
-    let quatro = parseInt(input.quatro);
-    let tres = parseInt(input.tres);
+import {
+  useFonts,
+  Lexend_600SemiBold,
+  Lexend_700Bold,
+} from '@expo-google-fonts/lexend';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-    console.log(valor);
-    if (valor > 1) {
-      console.log(valor);
-      let mult = dois * tres;
-      mult /= valor;
-      setInput({ ...input, result: mult.toString() });
-    } else if (valor == 1) {
-      let mult = dois * tres;
-      setInput({ ...input, result: mult.toString() });
-    } else {
-      let calcula = parseFloat(input.one);
-      let mult = dois * tres;
-      mult /= calcula;
-      console.log(calcula);
-      setInput({ ...input, result: mult.toString() });
-    }
-  }
+import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
+import { Theme } from './src/styles/Theme';
+export default function App() {
+  const [isFontsLoaded] = useFonts({
+    Lexend_600SemiBold,
+    Lexend_700Bold,
+  });
+  const { colors } = Theme;
   return (
-    <View style={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <View style={styles.items}>
-          <input
-            style={styles.input}
-            id="one"
-            type="text"
-            value={input.one}
-            onChange={handleInputChange}
-          />
-          <input
-            id="dois"
-            type="text"
-            value={input.dois}
-            onChange={handleInputChange}
-          />
-        </View>
-        <View style={styles.items}>
-          <input
-            id="tres"
-            type="text"
-            value={input.tres}
-            onChange={handleInputChange}
-          />
-          <input
-            id="quatro"
-            readOnly
-            type="text"
-            value={input.quatro}
-            onChange={handleInputChange}
-          />
-        </View>
-        <input
-          id="result"
-          readOnly
-          type="text"
-          value={input.result}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Calcular</button>
-      </form>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar backgroundColor={colors.background} />
+      {isFontsLoaded ? <Home /> : <Loading />}
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    flexDirection: "collum",
-    margin: "auto",
-  },
-  items: {
-    display: "flex",
-    justifyContent: "space-between",
-    flexDirection: "row",
-  },
-});
